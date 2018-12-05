de1f8f918695b2f79d73a81445cebcc02e629e57
Matthew Finlayson <matthewbfinlayson@gmail.com>
2018-12-05 00:03:45 -0500

    added requirements.txt

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
-from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
